0b5948415ced4e7f90576d25cdde740633a6e42d
lqlys <lys.lukasz555@gmail.com>
2022-01-26 17:28:14 +0100

    changed roots

--- contracts/SecretStuff.sol
+++ contracts/SecretStuff.sol
@@ -25,9 +25,9 @@ contract SecretStuff is ERC721, Ownable, ReentrancyGuard {
     mapping(address => uint) earlyBirdMintsCount;
     mapping(address => uint) whitelistMintsCount;
 
-    bytes32 public genesisRoot = 0x33887c7962aa450a853b0f9b53dce744f6e85ab34d8ea33328ade270014b3553;
+    bytes32 public genesisRoot = 0x96c4abbbb9dd027813f2b8c2c4917b15bb46b3c8d595841148f2de33d0c4d6ae;
     bytes32 public earlyBirdRoot = 0x8a9eb10e5902f3de696d482e8d43be02d4c2925d8a13b1d91564556786cd53c8;
-    bytes32 public whitelistRoot = 0xbb21e0d08852df8f7ae4685b0e005935d8a63cecf1751bc03ab210d7b0daea4e;
+    bytes32 public whitelistRoot = 0xc8dcde46981152281c9debdf2cb2e67894f175ab4616c08a6fd0c08d22f70d38;
 
     string private _customBaseURI;
 
